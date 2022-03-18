1df313933b03324ed44fd45198da2aced7341fca
MharyJ <hary.juwiarto@gmail.com>
2022-03-18 20:40:55 +0700

    Menghapus navbar

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -15,9 +15,6 @@
                 <li class="nav-item">
                     <a class="nav-link {{ ($title === 'POST') ? 'active' : ''}}" href="/blog">Blog</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ ($title === 'POST') ? 'active' : ''}}" href="/blog">Blog</a>
-                </li>
             </ul>
         </div>
     </div>
