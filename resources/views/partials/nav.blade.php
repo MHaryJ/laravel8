5d9783a9455521f9ecdb9a192ac64d8e2bcc0e85
MharyJ <hary.juwiarto@gmail.com>
2022-03-18 20:47:10 +0700

    Menambahkan file navbar

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -15,6 +15,9 @@
                 <li class="nav-item">
                     <a class="nav-link {{ ($title === 'POST') ? 'active' : ''}}" href="/blog">Blog</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ ($title === 'POST') ? 'active' : ''}}" href="/blog">Blog</a>
+                </li>
             </ul>
         </div>
     </div>
